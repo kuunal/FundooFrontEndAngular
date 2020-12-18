cb0174b01314c0f6b9e933d427b80fb6111b3345
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-18 20:34:07 +0530

    [Kunal] ADD: Google alike register page

--- Fundoo/src/app/material/material.module.ts
+++ Fundoo/src/app/material/material.module.ts
@@ -3,13 +3,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRippleModule} from '@angular/material/core';
 
 
 const material = [
   MatButtonModule,
   MatFormFieldModule,
   MatInputModule,
-  MatSelectModule
+  MatSelectModule,
+  MatCheckboxModule,
+  MatRippleModule
 ];
   
 @NgModule({
