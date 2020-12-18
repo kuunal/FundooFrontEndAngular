18461b4ba4205b9ff1da1ecdcf36b50a5294a499
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-19 01:23:01 +0530

    [Kunal] ADD: Frontend for Login

--- Fundoo/src/app/app.module.ts
+++ Fundoo/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
