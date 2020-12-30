3eebb80a6a91b99ac8c6740056995fc06066f976
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-30 15:44:06 +0530

    [Kunal] ADD: Search feature for notes

--- Fundoo/src/app/components/home/search/search.component.ts
+++ Fundoo/src/app/components/home/search/search.component.ts
@@ -1,3 +1,4 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component, NgZone, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SharedDataServiceService } from 'src/app/services/data/shared-data-service.service';
@@ -20,7 +21,20 @@ export class SearchComponent implements OnInit {
     this.searchFilter = function (searchValue) {
       return function (note) {
         return Object.keys(note).some(function (key) {
-          return note[key].toString().includes(searchValue);
+          if (typeof note[key] === 'string')
+            return note[key].toString().includes(searchValue);
+          else if (note[key] instanceof Array) {
+            return note[key].forEach((element) => {
+              return Object.keys(element).some(function (innerKey) {
+                if (element[innerKey] === 'eqw') {
+                  console.log(
+                    element[innerKey].toString().includes(searchValue)
+                  );
+                }
+                return element[innerKey].toString().includes(searchValue);
+              });
+            });
+          }
         });
       };
     };
